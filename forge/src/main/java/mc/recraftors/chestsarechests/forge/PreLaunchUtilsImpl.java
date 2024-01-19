5a6bd1fb3a13ad22c31409cc80c04a0e720c145d
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-01-19 23:39:40 +0100

    Mixin plugin upgrade (bis)
    (cherry picked from commit 6bff563003b25b62e4a84a81ee2652b8c1f14e61)

--- forge/src/main/java/mc/recraftors/chestsarechests/forge/PreLaunchUtilsImpl.java
+++ forge/src/main/java/mc/recraftors/chestsarechests/forge/PreLaunchUtilsImpl.java
@@ -16,14 +16,15 @@ public final class PreLaunchUtilsImpl {
         String[] strings;
         Object o = opt.get();
         if (o instanceof String s) {
-            strings = s.split(",");
+            strings = s.split("(,|\\s(-+)\\s)");
         } else if (o instanceof String[] array) {
             strings = array;
         } else {
             return false;
         }
         for (String s : strings) {
-            if (s.toLowerCase().replace(' ', '_').replace('-', '_').equals(author)) {
+            s = s.trim().toLowerCase();
+            if (s.replace(' ', '_').replace('-', '_').equals(author)) {
                 return true;
             }
         }
