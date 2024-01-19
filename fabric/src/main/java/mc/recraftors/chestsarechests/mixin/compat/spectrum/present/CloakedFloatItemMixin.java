a088339907b01e1c3da5f9d4e5dc8589cba27d9d
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-01-19 01:58:44 +0100

    Spectrum gravity item compat impl

---
+++ fabric/src/main/java/mc/recraftors/chestsarechests/mixin/compat/spectrum/present/CloakedFloatItemMixin.java
@@ -0,0 +1,29 @@
+package mc.recraftors.chestsarechests.mixin.compat.spectrum.present;
+
+import de.dafuqs.spectrum.blocks.gravity.CloakedFloatItem;
+import de.dafuqs.spectrum.blocks.gravity.FloatItem;
+import mc.recraftors.chestsarechests.util.ContainerItemHelper;
+import mc.recraftors.chestsarechests.util.FallInContainer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(CloakedFloatItem.class)
+public abstract class CloakedFloatItemMixin extends FloatItem implements ContainerItemHelper {
+    CloakedFloatItemMixin(Settings settings, float gravityMod) {
+        super(settings, gravityMod);
+    }
+
+    @Override
+    public Direction chests$getFallDirection(ItemStack stack) {
+        return this.getGravityMod() < 0 ? Direction.DOWN : Direction.UP;
+    }
+
+    @Override
+    public boolean chests$onOpenTick(ItemStack stack, FallInContainer container, Direction direction, World world, Vec3d pos, Vec3d velocity) {
+        float f = -(1 / this.getGravityMod());
+        return ContainerItemHelper.super.chests$onOpenTick(stack, container, direction, world, pos, velocity.multiply(1, f, 1));
+    }
+}
