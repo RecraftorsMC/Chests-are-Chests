2b972e2244b73c04375c5f708c1d16b7918b6003
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-01-20 11:21:56 +0100

    Expanded storage barrel special drop

--- common/src/main/java/mc/recraftors/chestsarechests/mixin/compat/expanded_storage/present/BarrelBlockMixin.java
+++ common/src/main/java/mc/recraftors/chestsarechests/mixin/compat/expanded_storage/present/BarrelBlockMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,6 +21,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BarrelBlock.class)
 public abstract class BarrelBlockMixin {
+    @SuppressWarnings("DuplicatedCode")
     @Inject(
             method = "scheduledTick",
             at = @At(
@@ -37,11 +39,15 @@ public abstract class BarrelBlockMixin {
         }
         if (!c.chests$isOpen()) return;
         if (!state.getProperties().contains(Properties.FACING)) return;
-        if (state.get(Properties.FACING) == Direction.DOWN) {
-            Box box = new Box(pos.getX(), pos.getY()-.5d, pos.getZ(), pos.getX()+1d, pos.getY(), pos.getZ()+1d);
-            if (world.isSpaceEmpty(box)) {
-                ChestsAreChests.dropAllDown((Inventory) entity, entity);
-            }
+        if (!world.getGameRules().getBoolean(ChestsAreChests.getBarrelFall())) return;
+        if (!world.getGameRules().getBoolean(ChestsAreChests.getBarrelFallThrowableSpecial())) return;
+        Direction dir = state.get(Properties.FACING);
+        Box box = Box.of(pos.toCenterPos(), 1, .5, 1)
+                .offset(.75 * dir.getOffsetX(), .75 * dir.getOffsetY(), .75 * dir.getOffsetZ());
+        if (world.isSpaceEmpty(box)) {
+            Vec3d outPos = pos.toCenterPos().add(0.75 * dir.getOffsetX(), 0.75 * dir.getOffsetY(), 0.75 * dir.getOffsetZ());
+            Vec3d velocity = new Vec3d(0.05 * dir.getOffsetX(), 0.05 * dir.getOffsetY(), 0.05 * dir.getOffsetZ());
+            c.chests$fallOut(world, dir, (Inventory) entity, outPos, velocity);
         }
     }
 }
