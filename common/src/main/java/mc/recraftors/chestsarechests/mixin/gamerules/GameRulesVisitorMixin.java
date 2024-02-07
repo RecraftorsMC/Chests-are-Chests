7a9d1ebead5ff72ee36a08899cdae023df26ce50
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-02-07 09:35:18 +0100

    Added Unruled API dependency (removed localized additional gamerules)

--- common/src/main/java/mc/recraftors/chestsarechests/mixin/gamerules/GameRulesVisitorMixin.java
+++
@@ -1,9 +0,0 @@
-package mc.recraftors.chestsarechests.mixin.gamerules;
-
-import mc.recraftors.chestsarechests.util.GameRulesVisitor;
-import net.minecraft.world.GameRules;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(GameRules.Visitor.class)
-public interface GameRulesVisitorMixin extends GameRulesVisitor {
-}
