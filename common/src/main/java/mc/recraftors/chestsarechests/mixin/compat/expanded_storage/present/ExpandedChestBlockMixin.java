d794e5b1cf17a403d9a072cb5c3e5ced9fd9bc09
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-02-13 10:45:01 +0100

    Compat mixins renaming to avoid duplicate class names
    (cherry picked from commit 32b29b8656eb28a06bf6d7b915f4f3598c6ebf4e)

--- common/src/main/java/mc/recraftors/chestsarechests/mixin/compat/expanded_storage/present/ExpandedChestBlockMixin.java
+++ common/src/main/java/mc/recraftors/chestsarechests/mixin/compat/expanded_storage/present/ExpandedChestBlockMixin.java
@@ -1,14 +1,14 @@
 package mc.recraftors.chestsarechests.mixin.compat.expanded_storage.present;
 
-import compasses.expandedstorage.common.block.BarrelBlock;
+import compasses.expandedstorage.common.block.ChestBlock;
 import mc.recraftors.chestsarechests.ChestsAreChests;
 import mc.recraftors.chestsarechests.util.BlockOpenableContainer;
+import mc.recraftors.chestsarechests.util.BooleanHolder;
 import mc.recraftors.chestsarechests.util.FallInContainer;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.*;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,36 +16,36 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(BarrelBlock.class)
-public abstract class BarrelBlockMixin {
+@Mixin(ChestBlock.class)
+public abstract class ExpandedChestBlockMixin {
     @SuppressWarnings("DuplicatedCode")
     @Inject(
             method = "scheduledTick",
             at = @At(
                     value = "INVOKE",
-                    target = "Lcompasses/expandedstorage/common/block/entity/BarrelBlockEntity;updateViewerCount(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V",
+                    target = "Lcompasses/expandedstorage/common/block/entity/ChestBlockEntity;updateViewerCount(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V",
                     shift = At.Shift.BEFORE
             )
     )
     private void onScheduledTickBeforeUpdateViewerCountInjector(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
         BlockEntity entity = world.getBlockEntity(pos);
-        FallInContainer c = (FallInContainer) entity;
-        BlockOpenableContainer chested = c.chests$getContainer();
-        if (c.chests$isOpen() && chested.chests$isForcedOpened(world) && !chested.chests$shouldStayOpen(world)) {
-            c.chests$forceClose();
+        if (!(entity instanceof FallInContainer fallIn)) return;
+        BlockOpenableContainer c = fallIn.chests$getContainer();
+        if (!(fallIn instanceof BooleanHolder holder)) return;
+        if (fallIn.chests$isOpen() && c.chests$isForcedOpened(world) && !c.chests$shouldStayOpen(world)) {
+            fallIn.chests$forceClose();
         }
-        if (!c.chests$isOpen()) return;
-        if (!state.getProperties().contains(Properties.FACING)) return;
+        if (!fallIn.chests$isOpen()) return;
         if (!world.getGameRules().getBoolean(ChestsAreChests.getBarrelFall())) return;
         if (!world.getGameRules().getBoolean(ChestsAreChests.getBarrelFallThrowableSpecial())) return;
-        Direction dir = state.get(Properties.FACING);
+        Direction dir = holder.chests$getBool() ? Direction.DOWN : Direction.UP;
         Vec3d center = Vec3d.ofCenter(new Vec3i(pos.getX(), pos.getY(), pos.getZ()));
         Box box = Box.of(center, 1, .5, 1)
                 .offset(.75 * dir.getOffsetX(), .75 * dir.getOffsetY(), .75 * dir.getOffsetZ());
         if (world.isSpaceEmpty(box)) {
             Vec3d outPos = center.add(0.75 * dir.getOffsetX(), 0.75 * dir.getOffsetY(), 0.75 * dir.getOffsetZ());
             Vec3d velocity = new Vec3d(0.05 * dir.getOffsetX(), 0.05 * dir.getOffsetY(), 0.05 * dir.getOffsetZ());
-            c.chests$fallOut(world, dir, (Inventory) entity, outPos, velocity);
+            fallIn.chests$fallOut(world, dir, (Inventory) entity, outPos, velocity);
         }
     }
 }
