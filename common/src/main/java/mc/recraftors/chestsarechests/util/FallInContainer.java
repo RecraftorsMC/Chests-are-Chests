1143ef6abdbbfc7d451f6a7064f28ebdc66c2477
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-04-14 11:36:24 +0200

    Slight input boxes shift

--- common/src/main/java/mc/recraftors/chestsarechests/util/FallInContainer.java
+++ common/src/main/java/mc/recraftors/chestsarechests/util/FallInContainer.java
@@ -26,14 +26,15 @@ import java.util.function.BiConsumer;
  * Base container interface to be used for any container that should either
  * allow items falling in, falling out, or getting flung away.
  */
+@SuppressWarnings("unused")
 public interface FallInContainer {
     VoxelShape EMPTY = Block.createCuboidShape(0, 0, 0, 0, 0, 0);
     VoxelShape INSIDE = Block.createCuboidShape(0, 0, 0, 16, 16, 16);
     VoxelShape BESIDE_POSITIVE_X = Block.createCuboidShape(14, 0, 0, 24, 16, 16);
-    VoxelShape ABOVE = Block.createCuboidShape(0, 13, 0, 16, 23, 16);
+    VoxelShape ABOVE = Block.createCuboidShape(0, 10, 0, 16, 20, 16);
     VoxelShape BESIDE_POSITIVE_Z = Block.createCuboidShape(0, 0, 14, 16, 16, 24);
     VoxelShape BESIDE_NEGATIVE_X = Block.createCuboidShape(-8, 0, 0, 2, 16, 16);
-    VoxelShape BELOW = Block.createCuboidShape(0, -8, 0, 16, 2, 16);
+    VoxelShape BELOW = Block.createCuboidShape(0, -6, 0, 16, 4, 16);
     VoxelShape BESIDE_NEGATIVE_Z = Block.createCuboidShape(0, 0, -8, 16, 16, 2);
     VoxelShape AROUND_AGAINST = Block.createCuboidShape(-2, -2, -2, 18, 18, 18);
     VoxelShape AROUND = VoxelShapes.union(INSIDE, BESIDE_POSITIVE_X, ABOVE, BESIDE_POSITIVE_Z, BESIDE_NEGATIVE_X, BELOW, BESIDE_NEGATIVE_Z);
