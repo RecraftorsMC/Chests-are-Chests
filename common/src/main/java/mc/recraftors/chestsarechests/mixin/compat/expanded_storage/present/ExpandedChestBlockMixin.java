c807c0671306ed2597c35bb8eca575dde761651d
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-04-14 11:46:17 +0200

    Removed unused import statements

--- common/src/main/java/mc/recraftors/chestsarechests/mixin/compat/expanded_storage/present/ExpandedChestBlockMixin.java
+++ common/src/main/java/mc/recraftors/chestsarechests/mixin/compat/expanded_storage/present/ExpandedChestBlockMixin.java
@@ -1,18 +1,11 @@
 package mc.recraftors.chestsarechests.mixin.compat.expanded_storage.present;
 
 import compasses.expandedstorage.common.block.ChestBlock;
-import mc.recraftors.chestsarechests.ChestsAreChests;
-import mc.recraftors.chestsarechests.util.BlockOpenableContainer;
-import mc.recraftors.chestsarechests.util.BooleanHolder;
 import mc.recraftors.chestsarechests.util.FallInContainer;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.inventory.Inventory;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
